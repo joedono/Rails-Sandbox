4707a5c66fa5b4e3cb3d9403bfbcda1be643b399
joedono <fjzusi@gmail.com>
2019-06-14 22:52:28 -0400

    Test for edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # TODO: 
+      flash[:success] = "Profile Updated"
+      redirect_to @user
     else
       render 'edit'
     end
