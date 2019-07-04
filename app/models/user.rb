3417f8c3964b137bf1ae777bff28fa51d3e077e1
joedono <fjzusi@gmail.com>
2019-07-04 16:59:21 -0400

    All the BE support for followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,10 +19,22 @@ class User < ApplicationRecord
     allow_nil: true
 
   has_many :microposts, dependent: :destroy
+
   has_many :active_relationships,
     class_name: "Relationship",
     foreign_key: "follower_id",
     dependent: :destroy
+  has_many :passive_relationships,
+    class_name: "Relationship",
+    foreign_key: "followed_id",
+    dependent: :destroy
+
+  has_many :following,
+    through: :active_relationships,
+    source: :followed
+  has_many :followers,
+    through: :passive_relationships,
+    source: :follower
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
@@ -77,6 +89,18 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
